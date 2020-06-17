b722b1c35a5f183a90bad558ec45fecfbbcc2184
Cleiton Braga <cleitonbraga56@gmail.com>
2020-06-17 12:23:10 -0300

    Arquivos iniciais

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,7 @@
+export async function up(){
+    //Criar tabela
+} 
+
+export async function down(){
+    //(Voltar atras) Deletar a tabela
+}
---
+++ server/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import routes from './routes.mjs';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
